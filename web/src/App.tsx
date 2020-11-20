807f7ca174f13a66c9f76e872b351e3090741401
Felipe <felipe.pichl@hotmail.com>
2020-11-20 15:01:14 -0300

    ReactRouterDom for web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 
+import Routes from './routes';
+
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
-      <h1>Hello World</h1>
+      <Routes />
     </>
   );
 };
