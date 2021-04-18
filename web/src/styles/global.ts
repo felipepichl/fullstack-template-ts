7e7087e7127c65b6fd78252777abbc341f3fb55d
Felipe Pichl <felipe.pichl@hotmail.com>
2021-04-18 19:06:46 -0300

    Web - Add axios and global style

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -4,6 +4,16 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
+  }
+  body {
+    -webkit-font-smothing: antialiased;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+  button {
+    cursor: pointer;
   }
 `;
