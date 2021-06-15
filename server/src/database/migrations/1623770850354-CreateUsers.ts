853d18262ff50722285b0da9a537e3ec621bfb27
Felipe <felipe.pichl@hotmail.com>
2021-06-15 12:31:04 -0300

    Server - Doing users migration

---
+++ server/src/database/migrations/1623770850354-CreateUsers.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1623770850354 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
