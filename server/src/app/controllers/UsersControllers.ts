287b0ef4205b5d7ab1abe859140fad116b2fefc3
Felipe Pichl <felipe.pichl@hotmail.com>
2021-06-16 19:41:04 -0300

    Server - Done validations with Yup

--- server/src/app/controllers/UsersControllers.ts
+++ server/src/app/controllers/UsersControllers.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
 
 import { UsersServices } from '../services/UsersServices';
 
 class UsersController {
   public async store(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validations Failed' });
+    }
 
     const usersServices = new UsersServices();
 
